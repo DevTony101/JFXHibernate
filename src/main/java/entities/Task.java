1a13b25ecc6f36941bbe89c15e1dcf92c994fd10
DevTony101 <tonycortesano@gmail.com>
2020-01-31 20:25:43 -0500

    Adding entities

---
+++ src/main/java/entities/Task.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Tony Manjarres
+ */
+@Table(name = "tasks")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean completed;
+
+    @Column(name = "deadlineDate", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private User user;
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,63 @@
+package entities;
+
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Tony Manjarres
+ */
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column
+    private String civilStatus;
+
+    @Column(nullable = false)
+    private Boolean verified;
+
+    @Column(name = "created_at", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Task> tasks;
+}
