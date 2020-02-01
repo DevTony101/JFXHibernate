d84a8c775c947dfa1ad94a21238ee1cc14a7ceb1
DevTony101 <tonycortesano@gmail.com>
2020-02-01 15:11:12 -0500

    Adding view for creating a new user

---
+++ src/main/java/controllers/CreateUserController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Tony Manjarres
+ */
+public class CreateUserController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO
+    }    
+    
+}
