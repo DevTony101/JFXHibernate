2f7d370f1e43bc11b471e411a9ada729e3cf436f
DevTony101 <tonycortesano@gmail.com>
2020-01-31 20:27:58 -0500

    Adding a String constant to hold the path to the hibernate config file

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,6 +1,8 @@
-package utilities;
+package utils;
 
 public class Constants {
+    
     public static final String APP_TITLE = "JFXHibernate";
     public static final String FXML_HOME = "/fxml/FXMLHome.fxml";
+    public static final String HIBERNATE_XML = "/utils/hibernate.cfg.xml";
 }
