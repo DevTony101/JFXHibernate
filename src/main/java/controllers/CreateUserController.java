d58806fcab5d307e24183de7c66a7213648bc033
DevTony101 <tonycortesano@gmail.com>
2020-02-01 16:10:21 -0500

    Adding UI controls to capture the info

--- src/main/java/controllers/CreateUserController.java
+++ src/main/java/controllers/CreateUserController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -10,6 +15,21 @@ import javafx.fxml.Initializable;
  * @author Tony Manjarres
  */
 public class CreateUserController implements Initializable {
+    
+    @FXML
+    private JFXTextField fieldFName;
+
+    @FXML
+    private JFXTextField fieldLName;
+
+    @FXML
+    private JFXComboBox<String> cbCivilStatus;
+
+    @FXML
+    private JFXTextField fieldUsername;
+
+    @FXML
+    private JFXCheckBox cbAuto;
 
     /**
      * Initializes the controller class.
@@ -17,6 +37,13 @@ public class CreateUserController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO
+        initComboBox();
+        
     }    
     
+    private void initComboBox() {
+        String css = "-fx-font-family: Segoe UI; -fx-font-size: 14px;";
+        cbCivilStatus.setStyle(css);
+        cbCivilStatus.setItems(FXCollections.observableArrayList("Single", "Married"));
+    }
 }
