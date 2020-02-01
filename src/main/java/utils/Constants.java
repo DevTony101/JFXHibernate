2bb1ca667a39b697148547d93c0a264ff3134bfd
DevTony101 <tonycortesano@gmail.com>
2020-02-01 15:12:28 -0500

    Adding String constant to hold the path to the create user fxml

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,6 +3,8 @@ package utils;
 public class Constants {
     
     public static final String APP_TITLE = "JFXHibernate";
-    public static final String FXML_HOME = "/fxml/FXMLHome.fxml";
     public static final String HIBERNATE_XML = "/utils/hibernate.cfg.xml";
+    
+    public static final String FXML_HOME = "/fxml/FXMLHome.fxml";
+    public static final String FXML_CREATE_USER = "/fxml/FXMLCreateUser.fxml";
 }
