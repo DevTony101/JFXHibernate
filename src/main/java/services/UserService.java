d0c2d4ad9f53c27c048ef800b8dc234d6869dce4
DevTony101 <tonycortesano@gmail.com>
2020-02-01 21:24:32 -0500

    Adding service layer for the user entity

---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,36 @@
+package services;
+
+import entities.User;
+import java.util.Calendar;
+import lombok.NoArgsConstructor;
+import repositories.UserRepository;
+
+/**
+ *
+ * @author Tony Manjarres
+ */
+@NoArgsConstructor
+public class UserService {
+    
+    private final UserRepository repo = UserRepository.getInstance();
+    
+    public static UserService getInstance() {
+        return UserServiceHolder.INSTANCE;
+    }
+    
+    private static class UserServiceHolder {
+
+        private static final UserService INSTANCE = new UserService();
+    }
+    
+    public void saveUser(String fname, String lname, String username, String status) {
+        User user = new User();
+        user.setFirstName(fname);
+        user.setLastName(lname);
+        user.setUsername(username);
+        user.setCivilStatus(status);
+        user.setDate(Calendar.getInstance().getTime());
+        user.setVerified(Boolean.TRUE);
+        repo.save(user);
+    }
+}
