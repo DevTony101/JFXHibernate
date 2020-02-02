ad1caa7c6c11a5a23f61fa1c59f0da7a2bc61528
DevTony101 <tonycortesano@gmail.com>
2020-02-02 16:54:41 -0500

    Creating method to periodically update the main users list if a new one is created

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,16 +1,23 @@
 package controllers;
 
+import entities.User;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.jboss.logging.Logger;
+import services.UserService;
 
 import utils.*;
 
@@ -20,7 +27,14 @@ import utils.*;
  */
 public class HomeController implements Initializable {
 
-    private static Logger LOG = Logger.getLogger(HomeController.class);
+    private final static Logger LOG = Logger.getLogger(HomeController.class);
+
+    @FXML
+    private VBox vbUsers;
+
+    //
+    private final UserService uService = UserService.getInstance();
+    private static int ogCount = 0; // Original count of rows in the table users
 
     @FXML
     private void addNewUser(MouseEvent event) {
@@ -37,11 +51,34 @@ public class HomeController implements Initializable {
         } catch (IOException e) {
             LOG.error("Not found: " + Constants.FXML_CREATE_USER);
         }
-    } 
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Home Controller!");
+        updateList();
+    }
+
+    public void updateList() {
+        // Every 5 seconds
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(5000), (event) -> {
+            List<User> users = uService.getAllUsers();
+            if (users.size() != ogCount) try {
+                ogCount = users.size();
+                vbUsers.getChildren().clear();
+                for (User user : users) {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource(Constants.FXML_USER_ITEM));
+                    UserItemController controller = new UserItemController();
+                    loader.setController(controller);
+                    vbUsers.getChildren().add(loader.load());
+                    controller.setUser(user);
+                }
+            } catch (IOException e) {
+                LOG.error("Error: " + e.getMessage());
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 
 }
