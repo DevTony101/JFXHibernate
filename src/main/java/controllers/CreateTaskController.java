8b97c7833ecdca9e85059c54f593d98ed26f1a08
DevTony101 <tonycortesano@gmail.com>
2020-02-03 19:34:34 -0500

    Creating the method to gather up the info and create a new task

--- src/main/java/controllers/CreateTaskController.java
+++ src/main/java/controllers/CreateTaskController.java
@@ -1,16 +1,24 @@
 package controllers;
 
+import services.TaskService;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import entities.User;
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import lombok.Setter;
+import services.UserService;
 
 /**
  * FXML Controller class
@@ -27,13 +35,15 @@ public class CreateTaskController implements Initializable {
 
     @FXML
     private JFXDatePicker fieldDate;
-    
+
     @FXML
     private Label lblUser;
-    
+
     //
     @Setter
     private User user;
+    private final UserService uService = UserService.getInstance();
+    private final TaskService tService = TaskService.getInstance();
 
     /**
      * Initializes the controller class.
@@ -45,7 +55,15 @@ public class CreateTaskController implements Initializable {
 
     @FXML
     void createTask(MouseEvent event) {
-        
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        String title = fieldTitle.getText().trim();
+        String description = fieldDescription.getText().trim();
+        LocalDate localDate = fieldDate.getValue();
+        Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+        Date deadline = Date.from(instant);
+        tService.saveTask(title, description, deadline, user.getId());
+        stage.close();
     }
-    
+
 }
